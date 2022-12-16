9b43719bad819e06f2a0d37ef079530009b50fdf
roddrigolucas <rodrigolucassantoss@icloud.com>
2022-12-16 08:44:27 -0300

    mODIFICAÇAO 1
    EU ATUALIZEI O CODIGO

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template #Funcao render render_template
+from flask import Flask, render_template ,url_for,request #Funcao render render_template para retornar uma página
 
 app = Flask(__name__)
 
-@app.route('/')
-def principal():
-	frutas = ["Morango", "Uva", "Laranja", "Mamão", "Maçã", "Pêra", "Melão", "Abacaxi"]
-	return render_template("index.html",frutas=frutas)
+frutas =[]
+registros=[]
 
-@app.route('/sobre')
+@app.route('/',methods=["GET", "POST"])
+def principal():
+	#frutas = ["Morango", "Uva", "Laranja", "Mamão", "Maçã", "Pêra", "Melão", "Abacaxi"]
+	if request.method == "POST":
+		if request.form.get("fruta"):
+			frutas.append(request.form.get("fruta"))
+	return render_template("index.html", frutas=frutas)
+
+@app.route('/sobre', methods=["GET", "POST"])
 def sobre():
-	return render_template("sobre.html")
+	#notas = {"Fulano":5.0, "Beltrano":6.0, "Aluno": 7.0, "Sicrano":8.5, "Rodrigo":9.5}
+	if request.method == "POST":
+		if request.form.get("aluno") and request.form.get("nota"):
+			registros.append({"aluno": request.form.get("aluno"),"nota": request.form.get("nota")})
+	return render_template("sobre.html", registros=registros)
+
 
 #http://127.0.0.1:5000
 

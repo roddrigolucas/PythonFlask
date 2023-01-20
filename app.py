ae8a42a73420bfefe3cb6fcd7e031894521f946c
roddrigolucas <rodrigolucassantoss@icloud.com>
2023-01-20 15:51:18 -0300

    IMPLEMENTANDO EDIÇÃO E ATUALIZAÇÃO DE REGISTROS NO FLASK

--- app.py
+++ app.py
@@ -86,6 +86,14 @@ def cria_curso():
 @app.route('/<int:id>/atualiza_curso', methods=["GET", "POST"])
 def atualiza_curso(id):
 	curso = cursos.query.filter_by(id=id).first()
+	if request.method == 'POST':
+		nome = request.form["nome"]
+		descricao = request.form["descricao"]
+		ch = request.form["ch"]
+
+		cursos.query.filter_by(id=id).update({"nome":nome, "descricao":descricao, "ch":ch})
+		db.session.commit()
+		return redirect(url_for('lista_cursos'))
 	return render_template("atualiza_curso.html", curso=curso)
 
 if __name__ =="__main__":
